dee20781f9691d2922be4cd0ffc64a79c09932ba
pepperflavor <j8747j@gmail.com>
2023-02-08 11:42:28 +0900

    배포용

---
+++ src/admin/admin.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { MypageModule } from './mypage/mypage.module';
+import { AddmusicModule } from './addmusic/addmusic.module';
+
+@Module({
+    imports: [MypageModule, AddmusicModule],
+    controllers: [],
+    providers: []
+})
+export class AdminModule {}
