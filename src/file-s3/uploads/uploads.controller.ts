46c7bc9000c73906ee78a77983a1b98ec781a904
pepperflavor <j8747j@gmail.com>
2023-02-21 16:50:01 +0900

    배포 준비완료

--- src/file-s3/uploads/uploads.controller.ts
+++ src/file-s3/uploads/uploads.controller.ts
@@ -20,3 +20,4 @@ export class UploadsController {
     return url;
   }
 
+}
--- src/user/login/login.controller.ts
+++ src/user/login/login.controller.ts
@@ -3,11 +3,12 @@ import {Response} from 'express'
 import { AuthService } from '../../auth/auth.service';
 import { LocalAuthGuard } from '../../auth/local-auth.guard';
 import { userLoginDto } from '../user_dto/user-login.dto';
+import { PrismaService } from '../../prisma.service';
 
 
 @Controller('user')
 export class UserLoginController {
-    constructor(private authService: AuthService){}
+    constructor(private authService: AuthService, private prisma: PrismaService){}
     
     @Post('/login')
     async login(@Body() userLoginForm: userLoginDto, @Res() res:Response, @Headers() headers: any){
@@ -15,6 +16,16 @@ export class UserLoginController {
         const data = await this.authService.postUserinfo(userLoginForm.user_wallet);
         //res.header('Authorization', `Bearer ${token}`);
         res.json({data: data, token: token });
+    }
+
+    @Get()
+    async getinfo(){
+        const data = await this.prisma.user.findFirst({
+            where:{
+                user_grade: 1
+            }
+        })
+        return data
     }
 
 }
